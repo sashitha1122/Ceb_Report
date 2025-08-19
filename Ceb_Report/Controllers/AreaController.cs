32f18dba65a4e4d209fbe03352d3fa4d969cb2f0
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 09:36:38 +0530

    add area controller

---
+++ Ceb_Report/Controllers/AreaController.cs
@@ -0,0 +1,38 @@
+﻿using Ceb_Report.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceb_Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class AreaController : ControllerBase
+    {
+        private readonly IAreaRepository _areaRepository;
+
+        public AreaController(IAreaRepository areaRepository)
+        {
+            _areaRepository = areaRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var areas = await _areaRepository.GetAllAreasAsync();
+            return Ok(areas);
+        }
+
+        [HttpGet("db-check")]
+        public IActionResult CheckDatabaseConnection()
+        {
+            bool isConnected = _areaRepository.IsDatabaseConnected(out var errorMessage);
+            if (isConnected)
+                return Ok(new { connected = true });
+            else
+                return StatusCode(500, new { connected = false, error = errorMessage });
+        }
+
+    }
+
+}
