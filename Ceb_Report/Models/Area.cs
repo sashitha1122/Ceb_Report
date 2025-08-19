7cbf7146e6c3d2add6209e74a26e257df890c3df
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 09:42:31 +0530

    implement area model

---
+++ Ceb_Report/Models/Area.cs
@@ -0,0 +1,9 @@
+﻿namespace Ceb_Report.Models
+{
+    public class Area
+    {
+        public string area_code { get; set; } = string.Empty;
+        public string area_name { get; set; } = string.Empty;
+
+    }
+}
