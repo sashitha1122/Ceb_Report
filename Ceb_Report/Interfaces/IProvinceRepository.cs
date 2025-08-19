c59174f67fe25537bce60043798b8f67f3d6a039
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:11:15 +0530

    implement interfaces

---
+++ Ceb_Report/Interfaces/IProvinceRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ceb_Report.Models;
+
+namespace Ceb_Report.Interfaces
+{
+    public interface IProvinceRepository
+    {
+        Task<IEnumerable<Province>> GetAllProvincesAsync();
+       
+    }
+}
