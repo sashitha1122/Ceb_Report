800195336c0f6359ab1bb05ec880b9025c03a925
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:26:43 +0530

    implement calc_cycle controller

---
+++ Ceb_Report/Controllers/CalCycleController.cs
@@ -0,0 +1,25 @@
+﻿using Ceb_Report.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceb_Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalCycleController : ControllerBase
+    {
+        private readonly ICalCycleRepository _repository;
+
+        public CalCycleController(ICalCycleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRecent24Cycles()
+        {
+            var cycles = await _repository.GetLast24CalCyclesAsync();
+            return Ok(cycles);
+        }
+    }
+}
