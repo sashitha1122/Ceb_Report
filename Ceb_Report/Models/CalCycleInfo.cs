ecbef7cd9d76462fc6728e80faf6c6b32a88c87e
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:28:01 +0530

    implement calc_cycle model

---
+++ Ceb_Report/Models/CalCycleInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Ceb_Report.Models
+{
+    public class CalCycleInfo
+    {
+        public int CalCycle { get; set; }
+        public string MonthYear { get; set; } = string.Empty;
+    }
+}
