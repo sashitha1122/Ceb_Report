e281125889bdcd3fd0d02753d3a429a0eec03968
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:26:25 +0530

    implement province controller

---
+++ Ceb_Report/Controllers/ProvinceController.cs
@@ -0,0 +1,27 @@
+﻿using Ceb_Report.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceb_Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvinceController : ControllerBase
+    {
+        private readonly IProvinceRepository _provinceRepository;
+
+        public ProvinceController(IProvinceRepository provinceRepository)
+        {
+            _provinceRepository = provinceRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var provinces = await _provinceRepository.GetAllProvincesAsync();
+            return Ok(provinces);
+        }
+
+       
+    }
+}
