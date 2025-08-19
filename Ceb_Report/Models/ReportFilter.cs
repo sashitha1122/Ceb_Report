d7f3fa978ce1c3e861770b1ee07aaacd1f231d87
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:30:02 +0530

    implement report filter model

---
+++ Ceb_Report/Models/ReportFilter.cs
@@ -0,0 +1,13 @@
+﻿namespace Ceb_Report.Models
+{
+    public class ReportFilter
+    {
+        public string ReportType { get; set; } // "area", "province", "division", "entire"
+        public string CycleType { get; set; }  // "bill" or "calculation"
+        public string AreaCode { get; set; }
+        public string ProvinceCode { get; set; }
+        public string Region { get; set; }
+        public string NetType { get; set; }
+        public int CycleNumber { get; set; }
+    }
+}
