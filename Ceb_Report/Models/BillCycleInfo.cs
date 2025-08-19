325de92fd77bd403d9ec487e6df1c6748a8e2fee
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:12:12 +0530

    implemnt billcycle model

---
+++ Ceb_Report/Models/BillCycleInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Ceb_Report.Models
+{
+    public class BillCycleInfo
+    {
+        public int BillCycle { get; set; }
+        public string MonthYear { get; set; } = string.Empty;
+    }
+}
