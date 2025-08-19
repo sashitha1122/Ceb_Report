3bd1fa22380be5ec39326b49b05c0a1abd997121
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:27:08 +0530

    implement bill_cycle controller

---
+++ Ceb_Report/Controllers/BillCycleInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Ceb_Report.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceb_Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillCycleInfoController : ControllerBase
+    {
+        private readonly IBillCycleInfoRepository _repository;
+
+        public BillCycleInfoController(IBillCycleInfoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRecent24Cycles()
+        {
+            var cycles = await _repository.GetLast24BillCyclesAsync();
+            return Ok(cycles);
+        }
+    }
+}
