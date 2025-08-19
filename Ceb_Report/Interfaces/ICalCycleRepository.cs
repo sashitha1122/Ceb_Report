942792b2930a1f62bef2bf199cd1577df927f98f
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:11:27 +0530

    impelemnt interface

---
+++ Ceb_Report/Interfaces/ICalCycleRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ceb_Report.Models;
+
+namespace Ceb_Report.Interfaces
+{
+    public interface ICalCycleRepository
+    {
+        Task<IEnumerable<CalCycleInfo>> GetLast24CalCyclesAsync();
+    }
+}
