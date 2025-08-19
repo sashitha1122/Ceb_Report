4dcd4e5ce92132e351826af3bea3b034063b080d
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:11:38 +0530

    implement interface

---
+++ Ceb_Report/Interfaces/IBillCycleInfoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ceb_Report.Models;
+
+namespace Ceb_Report.Interfaces
+{
+    public interface IBillCycleInfoRepository
+    {
+        Task<IEnumerable<BillCycleInfo>> GetLast24BillCyclesAsync();
+    }
+}
