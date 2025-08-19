dee1ce72633e7f78d434c0b67734de3d95ec886a
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:28:26 +0530

    impelmnet province model

---
+++ Ceb_Report/Models/Province.cs
@@ -0,0 +1,8 @@
+﻿namespace Ceb_Report.Models
+{
+    public class Province
+    {
+        public string ProvCode { get; set; } = string.Empty;
+        public string ProvName { get; set; } = string.Empty;
+    }
+}
