a37b43f042452287dabac051a98df6e5a664c29e
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 10:26:12 +0530

    implement region controller

---
+++ Ceb_Report/Controllers/RegionController.cs
@@ -0,0 +1,25 @@
+﻿using Ceb_Report.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ceb_Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionController : ControllerBase
+    {
+        private readonly IRegionRepository _regionRepository;
+
+        public RegionController(IRegionRepository regionRepository)
+        {
+            _regionRepository = regionRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var regions = await _regionRepository.GetAllRegionsAsync();
+            return Ok(regions);
+        }
+    }
+}
