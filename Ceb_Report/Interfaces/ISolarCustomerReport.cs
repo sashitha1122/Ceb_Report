7dd04b370e549b8720255157ad99c3428dfd3249
sachinthacham <sachinthachamindu26@gmail.com>
2025-08-19 09:43:00 +0530

    update solar customer information interface

---
+++ Ceb_Report/Interfaces/ISolarCustomerReport.cs
@@ -0,0 +1,10 @@
+﻿using Ceb_Report.Models;
+
+namespace Ceb_Report.Interfaces
+{
+    public interface ISolarCustomerReport
+    {
+        Task<IEnumerable<ReportEntry>> GetReportDataAsync(ReportFilter filter);
+        Task<bool> IsDatabaseConnectedAsync();
+    }
+}
